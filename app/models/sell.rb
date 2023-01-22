b55c71ef52e70bf55eb893133db6350b50a2a87c
Jorge-Ortiz-Mata <ortiz.mata.jorge@gmail.com>
2023-01-22 21:30:44 +0000

    Installing chart_kick and rendering fist graph.

--- app/models/sell.rb
+++ app/models/sell.rb
@@ -1,4 +1,5 @@
 class Sell < ApplicationRecord
+  # scope :last_sells
   validates :quantity, presence: true
   validates :day, presence: true
   validates :book_name, presence: true
@@ -12,5 +13,15 @@ class Sell < ApplicationRecord
   def calculate_total
     book_price = Book.find_by(name: book_name).price
     self.total = book_price * quantity
+  end
+
+  def self.last_sells
+    array = Array.new
+
+    Sell.all.each_with_index do |sell, index|
+      array.push([index, sell.quantity])
+    end
+
+    array
   end
 end
