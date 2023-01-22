7df4f0e3fbabe05291042c9686c6945b887bb0d8
Jorge-Ortiz-Mata <ortiz.mata.jorge@gmail.com>
2023-01-22 23:09:13 +0000

    Displaying all sells.

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,2 +1,11 @@
 module BooksHelper
+  def revenue_from_sells
+    array = Array.new
+
+    Book.all.each do |book|
+      array.push([book.name, book.sells.sum(:total).round(2)])
+    end
+
+    array
+  end
 end
